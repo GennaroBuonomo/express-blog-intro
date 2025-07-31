ea62c38f1c162e6402e2ec962cb7db403b7565d7
GennaroBuonomo <gennarobuonomo66@gmail.com>
2025-07-31 15:32:59 +0200

    Feat inizio l' importazione di express

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-console.log("Hello World");
+//IMPORTO EXPRESS
+const express = require('express');
+//CHIAMO LA FUNZIONE EXPRESS E NE RESTITUISCO IL VALORE IN UNA VARIABILE APP
+const app = express();
+//DEFINISCO LA PORTA 3000
+const port = 3000;
