10a018636a6ceaca80bf9d1d99ddba2b1b1e8d22
GennaroBuonomo <gennarobuonomo66@gmail.com>
2025-07-31 15:44:28 +0200

    Feat deinisco l rotta e la porta

--- app.js
+++ app.js
@@ -4,3 +4,13 @@ const express = require('express');
 const app = express();
 //DEFINISCO LA PORTA 3000
 const port = 3000;
+
+//DEFINISCO LA ROTTA ENTRY POINT DELLA NOSTRA APPLICAZIONE
+app.get('/', (req, res) => {
+  console.log('Server del mio blog')
+});
+
+//DICHIARO ALLA NOSTRA APPLICAZIONE DI RESTARE IN ASCOLTO SULLA PORTA DEFINITA IN ALTO
+app.listen(port, () => {
+  console.log(`Server del mio blog`)
+})
