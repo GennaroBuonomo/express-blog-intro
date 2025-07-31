4dca5224f23ba42249588e07c8b7290dd5753e9c
GennaroBuonomo <gennarobuonomo66@gmail.com>
2025-07-31 17:14:05 +0200

    fix immagine

--- app.js
+++ app.js
@@ -24,31 +24,31 @@ app.get('/bacheca', (req, res) => {
     {
      titolo: "Jaws: Lo Squalo che ha cambiato il cinema",
      contenuto: "Un approfondimento sul film cult di Steven Spielberg che ha terrorizzato intere generazioni e  lanciato il concetto di blockbuster estivo.",
-     immagine: "imgs/jaws.jpeg",
+     immagine: "img/jaws.jpeg",
      tags: ["thriller", "classico", "animali", "anni70"]
     },
     {
      titolo: "Pirates of the Caribbean: La maledizione della Perla Nera",
      contenuto: "Analisi del primo film della saga che ha trasformato i pirati in icone pop, tra humor e  avventure soprannaturali.",
-     immagine: "https://example.com/immagini/pirates.jpg",
+     immagine: "img/pirates of the caribbean.jpeg",
      tags: ["avventura", "pirati", "fantasy", "disney"]
     },
     {
      titolo: "Spider-Man: Un eroe tra le ragnatele",
      contenuto: "Dal fumetto al grande schermo: come l’Uomo Ragno è diventato uno dei supereroi più amati di   sempre.",
-     immagine: "https://example.com/immagini/spiderman.jpg",
+     immagine: "img/spiderman.jpeg",
      tags: ["supereroi", "marvel", "azione", "fumetti"]
     },
     {
      titolo: "The Godfather: Il padrino della cinematografia",
      contenuto: "Un omaggio a uno dei capolavori assoluti della storia del cinema, tra famiglia, onore e crimine organizzato.",
-     immagine: "https://example.com/immagini/godfather.jpg",
+     immagine: "img/the godfather.jpeg",
      tags: ["mafia", "dramma", "anni70", "coppola"]
     },
     {
      titolo: "Zombie: Quando i morti camminano",
      contenuto: "Dai film di Romero alle serie TV moderne: l’evoluzione del genere zombie nell’immaginario collettivo.",
-     immagine: "https://example.com/immagini/zombie.jpg",
+     immagine: "img/zombie.jpeg",
      tags: ["horror", "apocalisse", "non-morti", "survival"]
     }
   ];
